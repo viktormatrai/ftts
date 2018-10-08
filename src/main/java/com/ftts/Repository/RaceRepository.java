0c59a094cfa141c5d46f5bcebb53de8ef8b5e44e
Viktor Matrai <viktor0matrai@gmail.com>
2018-10-08 13:06:45 +0200

    controller and repository for RACEs

---
+++ src/main/java/com/ftts/Repository/RaceRepository.java
@@ -0,0 +1,15 @@
+package com.ftts.Repository;
+
+import com.ftts.data.Race;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+@RestResource(path = "race")
+public interface RaceRepository extends JpaRepository<Race, Integer>  {
+    Race getById(@Param("id") Integer id);
+
+    @Query(value = "SELECT r from Race r WHERE raceName LIKE %:raceName%")
+    Race getByRaceName(@Param("raceName") String raceName);
+}
---
+++ src/main/java/com/ftts/controller/RaceController.java
@@ -0,0 +1,30 @@
+package com.ftts.controller;
+
+import com.ftts.Repository.RaceRepository;
+import com.ftts.data.Race;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RaceController {
+
+    @Autowired
+    RaceRepository raceRepository;
+
+    @GetMapping(value = "/races", produces = "application/json")
+    public ResponseEntity<List<Race>> getRaces(){
+        return new ResponseEntity<>(raceRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "races/{raceName}", produces = "application/json")
+    public ResponseEntity<Race> getRaceByName(@PathVariable("raceName") String raceName){
+        return new ResponseEntity<>(raceRepository.getByRaceName(raceName), HttpStatus.OK);
+    }
+
+}
